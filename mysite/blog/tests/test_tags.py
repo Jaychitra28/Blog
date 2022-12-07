f97b332402e0be5308579068ad486645e4b9a318
Jaychitra28 <jayachitra@testpress.in>
2022-12-07 17:21:20 +0530

    Refactor:Tag testcase

--- mysite/blog/tests/test_tags.py
+++ mysite/blog/tests/test_tags.py
@@ -1,17 +1,39 @@
 from django.test import TestCase
 from blog.templatetags import blog_tags
 from blog.models import Post, Comment
-from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
+from blog.tests.test_modelmixintestcase import ModelMixinTestCase
 
 
 class TestTemplateTags(ModelMixinTestCase, TestCase):
-    def test_count_tag(self):
-        self.assertEqual(blog_tags.total_posts(), 2)
+    def test_total_post_tag_returns_total_number_of_posts(self):
+        self.assertEqual(blog_tags.total_posts(), 1)
 
     def test_latest_post_post_tag(self):
-        latestpost = blog_tags.show_latest_posts()
-        self.assertEquals(latestpost["latest_posts"][0].title, "Two")
+        post = Post.objects.create(
+            title="latest",
+            author=self.user,
+            body="Testing Published",
+            status="published",
+            slug="published",
+        )
+        self.assertEqual(
+            post.title,
+            blog_tags.show_latest_posts()["latest_posts"].first().title,
+        )
 
     def test_most_commented_post_tag(self):
-        most_commented_posts = blog_tags.get_most_commented_posts()
-        self.assertEqual(most_commented_posts[0].title, "Two")
+
+        most_commented_post = self.create_commented_posts(
+            self.published_post, 10
+        )
+        self.assertEqual(
+            blog_tags.get_most_commented_posts().first().title,
+            most_commented_post.title,
+        )
+
+    def test_post_without_comments(self):
+
+        post_without_comment = self.create_published_posts(1)
+        self.assertTrue(
+            post_without_comment not in blog_tags.get_most_commented_posts()
+        )
